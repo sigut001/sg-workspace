8d196c9c01eab77ef9649a1918ba9ed21bf01406
Simon Guth <simon.guth@outlook.de>
2025-03-02 09:28:46 +0100

    feat: add new ui-components-library

---
+++ libs/ui-components/src/lib/ui-components.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'ui-components',
+  imports: [CommonModule],
+  template: `<p>UiComponents works!</p>`,
+  styles: ``,
+})
+export class UiComponentsComponent {}
